71e60e7b855aeddaf20a5d66da146d7c1b10f60b
Isaac Berman <berman.isaac01@gmail.com>
2020-10-05 20:17:01 -0500

    game

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -1,10 +1,27 @@
 class tttGame:
-    board = [0,1,0,1,0,1,0,1,0,1]
+    board = [0,0,0,0,0,0,0,0,0,0]
     nextMove = 1
 
-    def makeMove(self):
-        square = input()
-        print(square)
+    def makeUserMove(self):
+        square = int(input())
+        self.board[square] = self.nextMove
+        self.nextMove *= -1
+        self.print()
+
+    def checkWin(self):
+        if self.board[0] == self.board[1] == self.board[2] or
+           self.board[0] == self.board[4] == self.board[8] or
+           self.board[0] == self.board[3] == self.board[6]
+           return self.board[0]
+
+        if self.board[6] == self.board[7] == self.board[8] or
+           self.board[1] == self.board[4] == self.board[7]
+           return self.board[7]
+
+        if self.board[3] == self.board[4] == self.board[5] or
+           self.board[2] == self.board[5] == self.board[8] or
+           self.board[3] == self.board[5] == self.board[6]):
+           return self.board[5]
 
     def print(self):
         for i in range(3):
@@ -21,8 +38,8 @@ class tttGame:
 def main():
     game = tttGame()
     game.print()
-    game.makeMove()
-    print('fuck')
+    game.makeUserMove()
+    game.makeUserMove()
 
 
 if(__name__ == "__main__"):
