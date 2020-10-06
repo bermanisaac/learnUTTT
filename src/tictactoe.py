aff971a47698b8293ea202388d4e00b6aacf705d
Isaac Berman <berman.isaac01@gmail.com>
2020-10-06 09:01:36 -0500

    tttGame working on command line

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -9,37 +9,38 @@ class tttGame:
         self.print()
 
     def checkWin(self):
-        if self.board[0] == self.board[1] == self.board[2] or
-           self.board[0] == self.board[4] == self.board[8] or
-           self.board[0] == self.board[3] == self.board[6]
+        if (self.board[0] == self.board[1] == self.board[2]) or\
+           (self.board[0] == self.board[4] == self.board[8]) or\
+           (self.board[0] == self.board[3] == self.board[6]):
            return self.board[0]
 
-        if self.board[6] == self.board[7] == self.board[8] or
-           self.board[1] == self.board[4] == self.board[7]
+        if (self.board[6] == self.board[7] == self.board[8]) or\
+           (self.board[1] == self.board[4] == self.board[7]):
            return self.board[7]
 
-        if self.board[3] == self.board[4] == self.board[5] or
-           self.board[2] == self.board[5] == self.board[8] or
-           self.board[3] == self.board[5] == self.board[6]):
+        if (self.board[3] == self.board[4] == self.board[5]) or\
+           (self.board[2] == self.board[5] == self.board[8]) or\
+           (self.board[3] == self.board[5] == self.board[6]):
            return self.board[5]
 
     def print(self):
         for i in range(3):
-            print(self.board[i], end = '')
+            print(self.board[i], end = '\t')
         print('')
         for i in range(3,6):
-            print(self.board[i], end = '')
+            print(self.board[i], end = '\t')
         print('')
         for i in range(6,9):
-            print(self.board[i], end = '')
+            print(self.board[i], end = '\t')
         print('')
 
 
 def main():
     game = tttGame()
     game.print()
-    game.makeUserMove()
-    game.makeUserMove()
+    result = 0
+    while result := game.checkWin() == 0:
+        game.makeUserMove()
 
 
 if(__name__ == "__main__"):
